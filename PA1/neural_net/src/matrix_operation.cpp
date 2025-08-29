485c464f2d2e401aacd15c22626c47e6f1840454
MohitThorat <mohitthorat777@gmai.com>
2025-08-29 21:54:06 +0530

    feat: reorder, unroll

--- PA1/neural_net/src/matrix_operation.cpp
+++ PA1/neural_net/src/matrix_operation.cpp
@@ -38,8 +38,16 @@ Matrix MatrixOperation::ReorderedMatMul(const Matrix& A, const Matrix& B) {
 	Matrix C(n,m);
 	
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+    int a;
 
+	for(int l = 0; l < k; l++) {
+		for(int i = 0; i < n ; i++) {
+			a = A(i,l);
+			for (int j = 0 ; j < m ; j++) {
+					C(i,j) +=  a * B(l,j);
+			}
+		}
+	}
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -60,7 +68,26 @@ Matrix MatrixOperation::UnrolledMatMul(const Matrix& A, const Matrix& B) {
 
     const int UNROLL = 4;
 //----------------------------------------------------- Write your code here ----------------------------------------------------------------
-    
+	int a, l;
+	const int k_unroll_aligned = k - (k % UNROLL);
+
+	for(int i = 0; i < n ; i++) {
+		for (int j = 0 ; j < m; j++) {
+
+			for(l = 0; l < k_unroll_aligned; l+= UNROLL) {
+				C(i,j) +=  
+				A(i,l) * B(l,j) 
+				+ A(i,l + 1) * B(l + 1,j) 
+				+ A(i,l + 2) * B(l + 2,j) 
+				+ A(i,l + 3) * B(l + 3,j);
+			}
+
+			// leftover
+			for (; l < k; l++) {
+				C(i,j) +=  A(i,l) * B(l,j);
+			}
+		}
+	}
 
 //-------------------------------------------------------------------------------------------------------------------------------------------
 
