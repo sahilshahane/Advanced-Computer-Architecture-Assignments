f4b49b33a0f5ace40d91aac2ca22498054f0b57e
sahilshahane <sahilshahane123@gmail.com>
2025-08-31 17:08:19 +0530

    feat: matrix size

--- PA1/Task1/reports_sahil/Task_1C/MatrixSIze_Instructions.py
+++ PA1/Task1/reports_sahil/Task_1C/MatrixSIze_Instructions.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 import sys
 import os
 
-def plot_matrix_instructions(file_path, output_file="output.png"):
-    # Read CSV (comma-separated)
-    df = pd.read_csv(file_path, sep=",")
-
-    # Extract only required columns
-    result = df[["MatrixSize", "Instructions"]]
-    result["Instructions"] = result["Instructions"] / 10000
-
-
-    # Plot
+def plot_matrix_instructions(files_with_labels, output_file="output.png"):
     plt.figure(figsize=(8, 5))
-    plt.plot(result["MatrixSize"], result["Instructions"], marker="o", linestyle="-")
+
+    for file_path, label in files_with_labels:
+        # Read CSV (comma-separated)
+        df = pd.read_csv(file_path, sep=",")
+
+        # Extract only required columns
+        result = df[["MatrixSize", "Instructions"]]
+        result["Instructions"] = result["Instructions"] / 10000
+
+        # Plot
+        plt.plot(result["MatrixSize"], result["Instructions"], marker="o", linestyle="-", label=label)
+
     plt.xlabel("Matrix Size")
-    plt.ylabel("Instructions")
+    plt.ylabel("Instructions (divided by 10000)")
     plt.title("Matrix Size vs Instructions")
     plt.grid(True)
 
@@ -24,22 +26,24 @@ def plot_matrix_instructions(file_path, output_file="output.png"):
     plt.ticklabel_format(style='plain', axis='y')
 
     # Show more ticks on Y-axis
-    plt.locator_params(axis="y", nbins=15)  # increase number of ticks
+    plt.locator_params(axis="y", nbins=15)
 
-    plt.text(
-        0.5, -0.15, 
-        "Note: Instructions are divided by 10000", 
-        ha="center", va="center", transform=plt.gca().transAxes, fontsize=9
-    )
+    # Add legend for labels
+    plt.legend()
 
     # Save plot
-    plt.savefig(output_file, dpi=300, bbox_inches="tight")
+    plt.savefig(output_file, dpi=500, bbox_inches="tight")
     print(f"Plot saved as {os.path.abspath(output_file)}")
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <csv_file> [output_file]")
-    else:
-        file_path = sys.argv[1]
-        output_file = sys.argv[2] if len(sys.argv) > 2 else "output.png"
-        plot_matrix_instructions(file_path, output_file)
+
+    file1, label1 = "reports_sahil/Task_1C/simd_128_average.csv", "128 Bit"
+    file2, label2 = "reports_sahil/Task_1C/simd_256_average.csv", "256 Bit"
+    file3, label3 = "reports_sahil/Task_1C/simd_512_average.csv", "512 Bit"
+    file4, label4 = "reports/naive/naive_average.csv", "Naive"
+
+    output_file = "reports_sahil/Task_1C/simd_MatrixSize_Instructions.png"
+
+    files_with_labels = [(file1, label1), (file2, label2), (file3, label3), (file4, label4)]
+    plot_matrix_instructions(files_with_labels, output_file)
